7c0ac00f407276a72c01f1bf9402db8be3c94c4e
Murad <muradrashidov995@gmail.com>
2023-11-01 14:51:59 +0400

    x

--- Todolist/app.js
+++ Todolist/app.js
@@ -7,8 +7,7 @@ let todos = [];
 
 function showTodos(){
     
-     todoInput.value &&   todos.push(todoInput.value);
-    //console.log(todos);
+   todoInput.value &&   todos.push(todoInput.value);
    let elements =  todos.map(todo => `
    <li  class="d-flex justify-content-between align-items-center bg-dark text-light p-2 rounded-3 mb-1">
    <span>${todo}</span>
@@ -17,11 +16,10 @@ function showTodos(){
    <p onclick=updateTodo(event) class="m-0 p-0"> ✏</p>
    <p onclick=fulfilTodo(event) class="m-0 p-0"> ✔</p>
    <div>
-   
    </li>
    `)
-       todoList.innerHTML =   ""
-
+   
+   todoList.innerHTML =   ""
    elements.forEach(el => {
     todoList.innerHTML += el
    })
@@ -34,22 +32,15 @@ function delTodo(e){
     let deletedItem = e.target.parentElement.previousElementSibling.textContent;
     todos = todos.filter(todo => todo != deletedItem)
     showTodos();
-        console.log(todos);
-
-    
 }
 function fulfilTodo (e){
-//console.log(e.target.parentElement.previousElementSibling.style);
 let target = e.target.parentElement.previousElementSibling.style;
 target.color = "lime";
-target.textDecoration = "line-through"
-
-
+target.textDecoration = "line-through";
 }
 function updateTodo(e){
     let updatedItem = e.target.parentElement.previousElementSibling.textContent; 
-       let newValue = prompt("yeni deger yazin",updatedItem)
-
+    let newValue = prompt("yeni deger yazin",updatedItem)
     todos.splice(todos.indexOf(updatedItem),1,newValue)
     showTodos();
 }
